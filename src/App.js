2698f47efe5667840e445478dfcb288d75730d8d
shrk19 <shreyakri19@gmail.com>
2022-07-11 02:07:39 +0530

    all routes set and functions complete

--- src/App.js
+++ src/App.js
@@ -2,19 +2,48 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 export default class App extends Component {
 
   render() {
     return (
       <>
+      <Router>
       <Navbar/>
       <div className="container w-75" style={{margin: "auto"}}>
-      <News/>
+
+      <Routes>
+        <Route exact path="/" element={<News key="general" category="general"/> } />
+        <Route exact path="/business" element={<News key="business" category="business" /> } />
+        <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" /> } />
+        <Route exact path="/health" element={<News key="health"  category="health" /> } />
+        <Route exact path="/science" element={<News key="science" category="science" /> } />
+        <Route exact path="/sports" element={<News key="sports" category="sports" /> } />
+        <Route exact path="/technology" element={<News key="technology" category="technology" /> } />
+      </Routes>
+      
+
       </div>
+
+      </Router>
+      
       </>
     )
   }
 }
+
+
+
+
+
+
+
+
 
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
 
-  
-
-  render() {
-    let {title, context, imageUrl, url} = this.props;
+    render() {
+    let {title, context, imageUrl, url, author, publishedAt, source} = this.props;
     return (
 
-      <div className="card my-4" style={{width: "16rem", height: "30rem", margin: "auto"}}>
-        <img src={imageUrl} style={{height: "150px"}} className="card-img-top" alt="..."/>
+      <div className="card my-4" style={{ margin: "auto"}}>
+        <span classname="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source.name}</span>
+        <img src={imageUrl} style={{height: "180px"}} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title" >{title.slice(0,100)}...</h5>
           <p className="card-text">{context.slice(0, 160)}...</p>
+          <p classname="card-text"><small classname="text-muted">{author}</small></p>
+          <p classname="card-text"><small classname="text-muted">{(new Date(publishedAt).toUTCString())}</small></p>
         </div>
         <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-light w-75" style={{position: "relative", bottom: "12px", margin: "auto"}}>Read more</a>
       </div>     
