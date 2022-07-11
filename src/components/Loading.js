ae0e3a4559d13154ebab4b48fd3738014db4b189
shrk19 <shreyakri19@gmail.com>
2022-07-11 23:12:07 +0530

    successfully added infinite scroll, problem-horizontal scroll bar appears

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ import loading from './loading.gif'
 export default class Loading extends Component {
   render() {
     return (
-      <div className='text-center'>
+      <div className='text-center my-5'>
         <img src={loading} alt="loading" />
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -2,40 +2,53 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import PropTypes from 'prop-types'
 import Loading from './Loading';
-// loading is not working
+import InfiniteScroll from "react-infinite-scroll-component";
+ 
 
 export default class News extends Component {
 
   static defaultProps = {
     country: 'in', 
     pageSize: 3, 
-    category: 'general'
+    category: 'general', 
   }
   static propTypes = {
     country: PropTypes.string, 
     pageSize: PropTypes.number,
-    category: PropTypes.string
+    category: PropTypes.string, 
   }
 
   constructor(){
     super();
     this.state = {
       articles : [],
-      loading : false, 
+      loading : true, 
       page : 1, 
       totalResults : 0
     };
   }
+    
+  fetchMoreData = async() => {
+
+    this.setState({page: this.state.page+1});
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ada07c9b6102417db6a2b0f185fa084c&page=${this.state.page}&pageSize=30`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+          totalResults: parsedData.totalResults,
+          articles: this.state.articles.concat(parsedData.articles), 
+        });
+
+  };
 
     async updateNews(){
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ada07c9b6102417db6a2b0f185fa084c&page=${this.state.page}&pageSize=30`;
-        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
           totalResults: parsedData.totalResults,
           articles: parsedData.articles, 
-          loading : false
+          loading: false      
         });
     }
 
@@ -62,24 +75,38 @@ export default class News extends Component {
 
   render() {   
     return (
-      <div>
+      <>
         <h2>Top Headlines</h2>
-        {(this.state.loading) &&  <Loading/>}
+        {this.state.loading && <Loading/>}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Loading/>}
+          >
         
-        <div className="container">
+          <div className="container">
+          
+          
           <div className="row">
-          {!this.state.loading && this.state.articles.map((element)=>
+
+          
+          {this.state.articles.map((element)=>
             {return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title!==null?element.title:""} context={element.description!==null?element.description:""} imageUrl={element.urlToImage!==null?element.urlToImage:"https://www.encompass-inc.com/wp-content/uploads/2019/07/july-2019-news-and-updates-encompass-solutions-Copy.jpg" } url={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source}/>
             </div>}
           )}
+          
           </div>
-        </div>
-        <div className="container d-flex justify-content-between">
+          
+          </div>
+          </InfiniteScroll>
+        
+        {/* <div className="container d-flex justify-content-between">
         <button type="button" className="btn btn-primary my-3" onClick={this.handlePrevClick} disabled={this.state.page<=1}>&larr; Previous</button>
         <button type="button" className="btn btn-primary my-3" onClick={this.handleNextClick} disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/30)}>Next &rarr;</button>
-        </div>
-        </div>
+        </div> */}
+        </>
     )
   }
 }
